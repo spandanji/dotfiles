349da2353342c58fc4eb06fcef0d0ad84e96493f
spandan2 <spandanghosh2@gmail.com>
2019-01-13 09:59:18 +0530

    Updates

--- .config/i3/conky.sh
+++ .config/i3/conky.sh
@@ -1,3 +1,3 @@
-conky -c '/home/spandan/.conky/TeejeeTech/CPU Panel (6-core)'
+conky -c '/home/spandan/.conky/TeejeeTech/CPU Panel (8-core)'
 conky -c '/home/spandan/.conky/Green Apple Desktop/Gotham'
 
--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -1,3 +1,6 @@
-# No widgets enabled!
-
-exit 0
+sleep 20s
+killall conky
+cd "/home/spandan/.conky/Green Apple Desktop"
+conky -c "/home/spandan/.conky/Green Apple Desktop/Gotham" &
+cd "/home/spandan/.conky/TeejeeTech"
+conky -c "/home/spandan/.conky/TeejeeTech/CPU Panel (8-core)" &
