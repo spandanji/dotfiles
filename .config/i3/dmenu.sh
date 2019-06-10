7a6cb52edd75c8f7c4c558a571212f69e9d2252a
spandan2 <spandanghosh2@gmail.com>
2019-06-11 00:11:42 +0530

    Rice complete

--- .config/i3/dmenu.sh
+++ .config/i3/dmenu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-FONT="Share Tech Mono 10"
-BACKGROUND_COLOR="#0c0c0c"
-FOREGROUND_COLOR="#888888"
+#-nb '#151515' -nf '#999999'
+FONT="Share Tech Mono 15"
+BACKGROUND_COLOR= "#151515"#0c0c0c"
+FOREGROUND_COLOR="##999999"#888888"
 BORDER_COLOR="#193b6c"
 
 rofi -show run -font "$FONT" -fg $FOREGROUND_COLOR -bg $BACKGROUND_COLOR -hlfg $BACKGROUND_COLOR -hlbg 
